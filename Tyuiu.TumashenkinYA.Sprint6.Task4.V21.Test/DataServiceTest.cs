83ff624befa0b10d90e0eaf572f01f9a9ee98185
eborius321 <eborius321@gmail.com>
2025-12-03 16:26:37 +0500

    Task4

---
+++ Tyuiu.TumashenkinYA.Sprint6.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.TumashenkinYA.Sprint6.Task4.V21.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint6.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double[] res = new double[11];
+            res = ds.GetMassFunction(startValue, stopValue);
+            double[] wait = new double[] { 28.05, 22.27, 17.01, 12.35, 7.51, 0, 1.62, -7.03, -12.99, -17.55, -21.43 };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TumashenkinYA.Sprint6.Task4.V21/FormMain.cs
@@ -0,0 +1,97 @@
+﻿using Tyuiu.TumashenkinYA.Sprint6.Task4.V21.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint6.Task4.V21
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+        private void bettonDone_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int startStep = Convert.ToInt32(textBoxStart_RKN.Text);
+                int stopStep = Convert.ToInt32(textBoxStop_RKN.Text);
+
+                int len = ds.GetMassFunction(startStep, stopStep).Length;
+
+                double[] valueArray;
+                valueArray = new double[len];
+
+                valueArray = ds.GetMassFunction(startStep, stopStep);
+
+                double[] xData = new double[len];
+                double[] yData = new double[len];
+
+                textBoxResult_RKN.Text = "";
+
+                for (int i = 0; i < len; i++)
+                {
+                    xData[i] = startStep + i;
+                    yData[i] = valueArray[i];
+                    textBoxResult_RKN.AppendText(valueArray[i] + Environment.NewLine);
+                }
+
+                formsPlotChartResult_RKN.Plot.Clear();
+
+                var scatter = formsPlotChartResult_RKN.Plot.Add.Scatter(xData, yData);
+
+                formsPlotChartResult_RKN.Plot.Title("Ãðàôèê ôóíêöèè");
+                formsPlotChartResult_RKN.Plot.XLabel("Îñü X");
+                formsPlotChartResult_RKN.Plot.YLabel("Îñü Y");
+
+                formsPlotChartResult_RKN.Refresh();
+            }
+
+            catch
+            {
+                MessageBox.Show("Ââåäåíû íåâåðíûå äàííûå", "Îøèáêà", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask4V21.txt";
+                File.WriteAllText(path, textBoxResult_RKN.Text);
+
+                DialogResult dialogResult = MessageBox.Show("Ôàéë" + path + "Ñîõðàíåí óñïåøíî!\nÎòêðûòü åãî â áëîêíîòå?", "Ñîîáùåíèå", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    System.Diagnostics.Process txt = new System.Diagnostics.Process();
+                    txt.StartInfo.FileName = "notepad.exe";
+                    txt.StartInfo.Arguments = path;
+                    txt.Start();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ñáîé ïðè ñîõðàíåíèè ôàéëà", "Îøèáêà", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 4 âûïîëíèë ñòóäåíò ãðóïïû ПКТб-25-1 Тумашенкин Ярослав Анатольевич", "Ñïðàâêà");
+        }
+        private void buttonResult_RKN_MouseDown(object sender, MouseEventArgs e)
+        {
+            buttonResult_RKN.BackColor = Color.Blue;
+        }
+
+        private void buttonResult_RKN_MouseEnter(object sender, EventArgs e)
+        {
+            buttonResult_RKN.BackColor = Color.Red;
+        }
+
+        private void buttonResult_RKN_MouseLeave(object sender, EventArgs e)
+        {
+            buttonResult_RKN.BackColor = Color.Green;
+        }
+    }
+}
